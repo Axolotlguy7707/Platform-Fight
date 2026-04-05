9118ea374fbfbfed52eba4379654e1ecd8bc07ac
Axolotlguy <practicleant1321@outlook.com>
2026-04-05 16:28:28 -0500

    enemy movement but better

--- src/objects/Mushroom.ts
+++ src/objects/Mushroom.ts
@@ -3,11 +3,13 @@ import Phaser from "phaser";
 export class Mushroom extends Phaser.Physics.Arcade.Sprite
 {
     speed!: number;
+
+    direction!: number;
     constructor(scene: Phaser.Scene, x: number, y: number)
     {
         super(scene, x, y, 'mushroom_idle', 0);
 
-        this.speed = 300;
+        this.speed = 100;
 
         scene.add.existing(this);
         scene.physics.add.existing(this);
@@ -15,12 +17,30 @@ export class Mushroom extends Phaser.Physics.Arcade.Sprite
         this.body?.setSize(30, 32);
         this.body?.setOffset(22, 32);
 
+        this.body.checkCollision.left = true;
+        this.body.checkCollision.right = true;
 
-        this.play('mushroom-idle');
+
+
+        this.play('mushroom-walk');
+
+        this.direction = -1;
     }
 
     update()
     {
-        this.setVelocityX(-this.speed);
+        this.setVelocityX(this.direction * this.speed);
+
+if (this.body?.blocked.left) {
+    this.direction = 1;
+    this.x += 2; // push away from wall
+}
+if (this.body?.blocked.right) {
+    this.direction = -1;
+    this.x -= 2;
+}
+
+
+
     }
 }
