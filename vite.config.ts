4ff13b44c4876617623a492a6548c79cebba80cd
Axolotlguy <practicleant1321@outlook.com>
2026-04-07 21:04:44 -0500

    build stuff, nw.js stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // Ensure assets are processed
   assetsInclude: ['**/*.png', '**/*.json', '**/*.mp3'],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        game: 'game.html'
+      }
+    }
+  }
 })
